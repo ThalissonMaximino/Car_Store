16d345083f799309a89fa897363f2247d05eb289
ThalissonMaximino <thalissonmaximino@gmail.com>
2023-10-17 13:16:10 -0300

    Add:BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import GlobalStyles from "./Styles/GlobalStyles"
+import { RoutesMain } from "./routes"
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
      <GlobalStyles/>
+     <RoutesMain/>
     </>
   )
 }
