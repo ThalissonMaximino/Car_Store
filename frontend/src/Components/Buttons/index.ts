03048b5c138c81898426bc9b2f660e96fc73357e
ThalissonMaximino <thalissonmaximino@gmail.com>
2023-10-17 14:20:17 -0300

    Add:ButtonComponent

---
+++ frontend/src/Components/Buttons/index.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+interface ButtonProps {
+  $width?: number;
+  $size?: number;
+  $weight?: string;
+  $border?: boolean;
+  $color?: string;
+  $background?: string;
+  $display?: boolean;
+  $disable?: boolean;
+  $minWidth?: number;
+  $maxWidth?: number;
+}
+
+const Button = styled.button<ButtonProps>`
+  min-height: var(--button-height-1);
+  border-radius: var(--button-border);
+
+  border: ${({ $border }) => ($border ? "var(--border-button-1)" : "none")};
+
+  width: ${({ $width }) =>
+    $width ? `var(--button-width-${$width})` : "var(--button-width-1)"};
+
+  min-width: ${({ $minWidth }) =>
+    $minWidth ? `var(--button-width-${$minWidth})` : "none"};
+
+  max-width: ${({ $maxWidth }) =>
+    $maxWidth ? `var(--button-width-${$maxWidth})` : "none"};
+
+  background-color: ${({ $background }) =>
+    $background ? `var(--${$background})` : "var(--color-brand-brand-1)"};
+
+  color: ${({ $color }) => ($color ? `var(--${$color})` : "var(--color-grey-scale-grey-10)")};
+
+  font-size: ${({ $size }) =>
+    $size ? `var(--font-body-${$size})` : "var(--font-body-1)"};
+
+  font-weight: ${({ $weight }) =>
+    $weight ? `var(--${$weight})` : "var(--font-semibold)"};
+
+  @media (min-width: 1024px) {
+    display: ${({ $display }) => ($display ? "none" : "block")};
+  }
+
+  cursor: ${({ $disable }) => ($disable ? "none" : "pointer")};
+`;
+
+export default Button;
