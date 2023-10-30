6ddadff8ce5ea3c2d7e01b9b2a9296db0398a5fe
ThalissonMaximino <thalissonmaximino@gmail.com>
2023-10-30 00:20:08 -0300

    Add:Home Page and routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,14 +3,16 @@ import {Routes, Route} from "react-router-dom"
 import { Login } from "../Pages/Login"
 import { Dashboard } from "../Pages/Dashboard"
 import { Register } from "../Pages/Register"
+import Home from '../Pages/Home'
 
 
  const RoutesMain = () => {
 
     return(
         <Routes>
+            <Route path="/" element={<Home/>}/>
             <Route path="/register" element={<Register/>}/>
-            <Route path="/" element={<Login/>}/>
+            <Route path="/login" element={<Login/>}/>
             <Route path="/dashboard" element={<Dashboard/>}/>
         </Routes>
     )
